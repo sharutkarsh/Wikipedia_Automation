d09f9c12ebf6dc19f7a7b1d16f75bf005b031dc7
Utkarsh sharma <38309256+sharutkarsh@users.noreply.github.com>
2022-01-20 02:23:11 +0530

    Add files via upload
    add new proj

---
+++ src/main/java/DataResources.java
@@ -0,0 +1,32 @@
+
+public class DataResources {
+
+	String frames= "(//a[contains(text(),'Frames')])[1]";
+	
+	String FrameHeader = "//div/h3";
+	
+	String iFrameStr = "//a[contains(text(),'iFrame')]";
+	
+	String iFrameHeader = "//div/h3";
+	
+	String field1 = "//body[@id=\"tinymce\"]/p";
+	
+	String File ="//span[text()='File']";
+	
+	String Edit = "//span[text()='Edit']";
+	
+	String View = "//span[text()='View']";
+	
+	String Format = "//span[text()='Format']";
+	
+	String Formats ="//div[@title=\"Formats\"]";
+	
+	String Headings = "//div[text()='Headings']";
+	
+	String Heading1 = "//div/h1[text()='Heading 1']";
+	
+	String ElementalSelenium = "//a[text()='Elemental Selenium']";
+	
+	
+	
+}
